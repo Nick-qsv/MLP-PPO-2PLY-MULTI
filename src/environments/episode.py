bf4b77210194ae7066550b75f236b8fce03f7a9f
Nick-qsv <w0m85k27@protonmail.com>
2024-10-29 20:50:44 +0100

    reverted to old episode without padding

--- src/environments/episode.py
+++ src/environments/episode.py
@@ -23,28 +23,8 @@ class Experience:
 
 
 class Episode:
-    # Define a singleton padding experience to minimize memory usage
-    PADDING_EXPERIENCE = Experience(
-        observation=None,
-        action_mask=None,
-        action=None,
-        action_log_prob=None,
-        state_value=None,
-        reward=0.0,
-        done=True,
-        next_observation=None,
-        next_state_value=None,
-    )
-
     def __init__(self):
         self.experiences = []  # List of Experience objects
 
     def add_experience(self, experience):
         self.experiences.append(experience)
-
-    def pad_experience(self, max_len):
-        current_len = len(self.experiences)
-        num_padding = max_len - current_len
-        if num_padding > 0:
-            # Reuse the singleton padding experience to minimize memory usage
-            self.experiences.extend([self.PADDING_EXPERIENCE] * num_padding)
